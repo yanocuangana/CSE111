463e56bd53bacdd6e97c60118f8dcdbf80069396
yanocuangana <yanocuangana@gmail.com>
2023-06-20 18:50:52 +0100

    added

---
+++ Python/receipt.py
@@ -0,0 +1,85 @@
+from ast import If
+from asyncore import read
+import csv
+from itertools import product
+
+
+def main():
+    products_dict = read_dict("products.csv", 0)
+    print(products_dict)
+
+    with open("request.csv", "rt") as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+
+        # for the requested elements
+        REQUESTED_PRODUCT_INDEX = 0
+        REQUESTED_QUANTITY_INDEX = 1
+
+        # for the products_dict elements
+        PRODUCT_NAME_INDEX = 1
+        PRODUCT_COST_INDEX = 2
+        for row_list in reader:
+            requested_product_number = row_list[REQUESTED_PRODUCT_INDEX]
+            requested_quantity = row_list[REQUESTED_QUANTITY_INDEX]
+
+            for key, product in products_dict.items():
+                name = product[PRODUCT_NAME_INDEX]
+                cost = product[PRODUCT_COST_INDEX]
+                final_cost = float(cost) * float(requested_quantity)
+                if requested_product_number == key:
+                    print(
+                        f"Item: {name}, Quantity: {requested_quantity}, Price: ${cost}")
+
+
+def read_dict(filename, key_column_index):
+    """Read the contents of a CSV file into a compound
+    dictionary and return the dictionary.
+    Parameters
+        filename: the name of the CSV file to read.
+        key_column_index: the index of the column
+            to use as the keys in the dictionary.
+    Return: a compound dictionary that contains
+        the contents of the CSV file.
+    """
+    # Create an empty dictionary that will
+    # store the data from the CSV file.
+    dictionary = {}
+
+    # Open the CSV file for reading and store a reference
+    # to the opened file in a variable named csv_file.
+    with open(filename, "rt") as csv_file:
+
+        # Use the csv module to create a reader object
+        # that will read from the opened CSV file.
+        reader = csv.reader(csv_file)
+
+        # The first row of the CSV file contains column
+        # headings and not data, so this statement skips
+        # the first row of the CSV file.
+        next(reader)
+
+        # Read the rows in the CSV file one row at a time.
+        # The reader object returns each row as a list.
+        for row_list in reader:
+
+            # From the current row, retrieve the data
+            # from the column that contains the key.
+            # This will use the column that will be used as key
+            key = row_list[key_column_index]
+            price = row_list[2]
+
+            # Store the data from the current row
+            # into the dictionary.
+            dictionary[key] = row_list
+
+    # Return the dictionary.
+    return dictionary
+
+
+# If this file was executed like this:
+# > python teach_solution.py
+# then call the main function. However, if this file
+# was simply imported, then skip the call to main.
+if __name__ == "__main__":
+    main()
--- Python/water_flow.py
+++ Python/water_flow.py
@@ -1,44 +1,5 @@
-PVC_SCHED80_INNER_DIAMETER = 0.28687 # (meters)  11.294 inches
-PVC_SCHED80_FRICTION_FACTOR = 0.013  # (unitless)
-SUPPLY_VELOCITY = 1.65               # (meters / second)
+def water_column_height(tower_height, tank_wall):
 
-HDPE_SDR11_INNER_DIAMETER = 0.048692 # (meters)  1.917 inches
-HDPE_SDR11_FRICTION_FACTOR = 0.018   # (unitless)
-HOUSEHOLD_VELOCITY = 1.75            # (meters / second)
+    water_height = tower_height + (3 * tank_wall / 4)
 
-
-def main():
-    tower_height = float(input("Height of water tower (meters): "))
-    tank_height = float(input("Height of water tank walls (meters): "))
-    length1 = float(input("Length of supply pipe from tank to lot (meters): "))
-    quantity_angles = int(input("Number of 90° angles in supply pipe: "))
-    length2 = float(input("Length of pipe from supply to house (meters): "))
-
-    water_height = water_column_height(tower_height, tank_height)
-    pressure = pressure_gain_from_water_height(water_height)
-
-    diameter = PVC_SCHED80_INNER_DIAMETER
-    friction = PVC_SCHED80_FRICTION_FACTOR
-    velocity = SUPPLY_VELOCITY
-    reynolds = reynolds_number(diameter, velocity)
-    loss = pressure_loss_from_pipe(diameter, length1, friction, velocity)
-    pressure += loss
-
-    loss = pressure_loss_from_fittings(velocity, quantity_angles)
-    pressure += loss
-
-    loss = pressure_loss_from_pipe_reduction(diameter,
-            velocity, reynolds, HDPE_SDR11_INNER_DIAMETER)
-    pressure += loss
-
-    diameter = HDPE_SDR11_INNER_DIAMETER
-    friction = HDPE_SDR11_FRICTION_FACTOR
-    velocity = HOUSEHOLD_VELOCITY
-    loss = pressure_loss_from_pipe(diameter, length2, friction, velocity)
-    pressure += loss
-
-    print(f"Pressure at house: {pressure:.1f} kilopascals")
-
-
-if __name__ == "__main__":
-    main()
+    return water_height
