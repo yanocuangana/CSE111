e6ba774ab8ed3e3935cf38eae557f223707e022b
yanocuangana <yanocuangana@gmail.com>
2023-06-21 02:58:42 +0100

    added

--- Python/test_water_flow.py
+++ Python/test_water_flow.py
@@ -1,4 +1,4 @@
-from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe
+from water_flow import water_column_height, pressure_gain_from_water_height, pressure_loss_from_pipe, pressure_loss_from_fittings, reynolds_number, pressure_loss_from_pipe_reduction
 from pytest import approx
 import pytest
 
@@ -49,8 +49,46 @@ def test_pressure_loss_from_pipe():
     p = pressure_loss_from_pipe(0.28687, 1800.75,  0.013, 1.65)
     assert p == approx(-110.884 , abs= 0.001)
 
+def test_pressure_loss_from_fittings():
 
+    p = pressure_loss_from_fittings(0, 3)
+    assert p == approx(0, abs= 0.001)
 
+    p = pressure_loss_from_fittings(1.65, 0)
+    assert p == approx(0, abs= 0.001)
+
+    p = pressure_loss_from_fittings(1.65, 2)
+    assert p == approx(-0.109, abs= 0.001)
+
+    p = pressure_loss_from_fittings(1.75, 2)
+    assert p == approx(-0.122, abs= 0.001)
+
+    p = pressure_loss_from_fittings(1.75, 5)
+    assert p == approx(-0.306, abs= 0.001)
+
+def test_reynolds_number():
+
+    p = reynolds_number(0.048692, 0)
+    assert p == approx(0, abs= 1)
+
+    p = reynolds_number(0.048692, 1.65)
+    assert p == approx(80069, abs= 1)
+
+    p = reynolds_number(0.048692, 1.75)
+    assert p == approx(84922, abs= 1)
+
+    p = reynolds_number(0.28687, 1.65)
+    assert p == approx(471729, abs= 1)
+
+    p = reynolds_number(0.28687, 1.75)
+    assert p == approx(500318, abs= 1)
+
+def test_pressure_loss_from_pipe_reduction():
+
+    p = pressure_loss_from_pipe_reduction(0.28687, 0, 1, 0.048692)
+    assert p == approx(0, abs= 0.001)
+
+    
 
 
 # Call the main function that is part of pytest so that the
